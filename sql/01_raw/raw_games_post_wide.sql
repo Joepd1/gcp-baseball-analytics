ec178e2602e9579e3ad68e6b76cd23fbf9ca33fd
Joep Demollin <joep.demollin@student.kuleuven.be>
2025-04-11 23:37:46 +0200

    Add raw table queries for all tables in dataset

---
+++ sql/01_raw/raw_games_post_wide.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE TABLE
+    `devoteam-tech-challenge-456311.baseball.raw_games_post_wide` AS
+SELECT
+    *
+FROM
+    `bigquery-public-data.baseball.games_post_wide`
---
+++ sql/01_raw/raw_games_wide.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE TABLE
+    `devoteam-tech-challenge-456311.baseball.raw_games_wide` AS
+SELECT
+    *
+FROM
+    `bigquery-public-data.baseball.games_wide`
