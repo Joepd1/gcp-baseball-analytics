e6c6e2b892266685e3abc912782d2a5f1595abe7
Joep Demollin <joep.demollin@student.kuleuven.be>
2025-04-12 16:55:01 +0200

    Create stg_seasons: taking unique season data from schedules.

---
+++ sql/02_staging/stg_seasons.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE TABLE
+    `devoteam-tech-challenge-456311.baseball.stg_seasons` AS
+SELECT DISTINCT
+    seasonId,
+    year
+FROM
+    `devoteam-tech-challenge-456311.baseball.raw_schedules`
